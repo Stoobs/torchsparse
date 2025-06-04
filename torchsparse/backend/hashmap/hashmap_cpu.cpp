1857727b6d04e2307d1324d0a58b2785f5fb5f0e
stoo <stoo+git@stoo.org.uk>
2025-06-04 22:15:06 +0100

    Add google sparsehash changes.

--- torchsparse/backend/hashmap/hashmap_cpu.cpp
+++ torchsparse/backend/hashmap/hashmap_cpu.cpp
@@ -5,24 +5,37 @@
 #include <cstdlib>
 #include <stdexcept>
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4244) // conversion warnings
+#pragma warning(disable: 4267) // conversion warnings
+#endif
+
 void HashTableCPU::lookup_vals(const int64_t* const keys,
-                               int64_t* const results, const int n) {
+                             int64_t* const results, const int n) {
+#ifdef _MSC_VER
+    for (int idx = 0; idx < n; idx++) {
+#else
 #pragma omp parallel for
-  for (int idx = 0; idx < n; idx++) {
-    int64_t key = keys[idx];
-    google::dense_hash_map<int64_t, int64_t>::iterator iter = hashmap.find(key);
-    if (iter != hashmap.end()) {
-      results[idx] = iter->second;
-    } else {
-      results[idx] = 0;
+    for (int idx = 0; idx < n; idx++) {
+#endif
+        int64_t key = keys[idx];
+        auto iter = hashmap.find(key);
+        if (iter != hashmap.end()) {
+            results[idx] = iter->second;
+        } else {
+            results[idx] = 0;
+        }
     }
-  }
 }
 
 void HashTableCPU::insert_vals(const int64_t* const keys,
-                               const int64_t* const vals, const int n) {
-  for (int i = 0; i < 10; i++) {
-    printf("%d, %d, %d, %d\n", i, i < n, n, i < 10);
-    // hashmap[(int)keys[idx]] = (int)vals[idx]+1;
-  }
+                             const int64_t* const vals, const int n) {
+    for (int i = 0; i < n; i++) {
+        hashmap[keys[i]] = vals[i];
+    }
 }
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
--- torchsparse/backend/hashmap/hashmap_cpu.hpp
+++ torchsparse/backend/hashmap/hashmap_cpu.hpp
@@ -4,15 +4,25 @@
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
-#include <google/dense_hash_map>
 #include <vector>
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4244) // conversion warnings
+#pragma warning(disable: 4267) // conversion warnings
+#include <sparsehash/internal/sparseconfig.h>
+#endif
+
+#include <sparsehash/dense_hash_map>
 
 class HashTableCPU {
  private:
   google::dense_hash_map<int64_t, int64_t> hashmap;
 
  public:
-  HashTableCPU() {}
+  HashTableCPU() {
+    hashmap.set_empty_key(-1);  // Required for dense_hash_map
+  }
 
   ~HashTableCPU() {}
 
@@ -22,3 +32,7 @@ class HashTableCPU {
   void lookup_vals(const int64_t* const keys, int64_t* const results,
                    const int n);
 };
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif

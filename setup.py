1cdcd20a8c823f1a4df9adf09c70626f6577b5f5
stoo <stoo+git@stoo.org.uk>
2025-06-05 02:06:43 +0100

    fixing sparsehash issues still

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ define_macros = []
 include_dirs = [
     os.path.abspath("torchsparse/backend"),
     os.path.abspath("third_party/sparsehash/src"), # Keep for dense_hash_map etc.
+    os.path.abspath("third_party/sparsehash/src/sparsehash/internal")
 ]
 
 # Platform-specific compiler arguments
